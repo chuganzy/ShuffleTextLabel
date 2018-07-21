3c6f8d912e958112145dac7986d923984dc78a54
ferdibayrak <ferdi_bayrakk@hotmail.com>
2018-07-22 01:58:47 +0300

    Swift 4 Update

--- ShuffleTextLabel/ShuffleTextLabel.swift
+++ ShuffleTextLabel/ShuffleTextLabel.swift
@@ -8,9 +8,9 @@
 
 public class ShuffleTextLabel: UILabel {
     public var shuffleInterval = CFTimeInterval(0.05)
-    public var shuffleCharactors: String.CharacterView?
+    public var shuffleCharactors: String?
     public var shuffleTextLength: Int? = 3
-    public var shuffleSkipCharactorSet: NSCharacterSet? = NSCharacterSet.newlineCharacterSet()
+    public var shuffleSkipCharactorSet: NSCharacterSet? = NSCharacterSet.newlines as NSCharacterSet
     
     private weak var shuffleDisplayLink: CADisplayLink?
     private var shuffleBeginTime: CFTimeInterval?
@@ -18,8 +18,8 @@ public class ShuffleTextLabel: UILabel {
     
     public func beginShuffleText(text: String) {
         self.invalidateShuffleText()
-        let link = CADisplayLink(target: self, selector: "animateOneFrame:")
-        link.addToRunLoop(NSRunLoop.mainRunLoop(), forMode: NSRunLoopCommonModes)
+        let link = CADisplayLink(target: self, selector: #selector(animateOneFrame))
+        link.add(to: RunLoop.main, forMode: RunLoopMode.commonModes)
         self.shuffleBeginTime = CACurrentMediaTime()
         self.shuffleText = text
         self.shuffleDisplayLink = link
@@ -31,29 +31,29 @@ public class ShuffleTextLabel: UILabel {
     
     // MARK: Private
     @objc private func animateOneFrame(link: CADisplayLink) {
-        guard let beginTime = self.shuffleBeginTime, text = self.shuffleText else {
+        guard let beginTime = self.shuffleBeginTime, let text = self.shuffleText else {
             assertionFailure()
             return
         }
         let time = link.timestamp - beginTime
         let finishedIndex = Int(floor(time / self.shuffleInterval))
-        if text.characters.count <= finishedIndex {
+        if text.count <= finishedIndex {
             self.invalidateShuffleText()
         }
         
-        self.text = text.characters.enumerate().reduce("") { (current, pair) -> String in
+        self.text = text.enumerated().reduce("") { (current, pair) -> String in
             let index = pair.0
             let char = pair.1
             let shouldSkip = String(char).utf16.first.flatMap { self.shuffleSkipCharactorSet?.characterIsMember($0) } ?? false
             if index < finishedIndex || shouldSkip {
                 return current + String(char)
             }
-            if let shuffleTextLength = self.shuffleTextLength where shuffleTextLength + finishedIndex < index {
+            if let shuffleTextLength = self.shuffleTextLength, shuffleTextLength + finishedIndex < index {
                 return current
             }
-            let chars = self.shuffleCharactors ?? text.characters
+            let chars = self.shuffleCharactors ?? text
             let shuffleIndex = Int(arc4random_uniform(UInt32(chars.count)))
-            return current + String(chars[chars.startIndex.advancedBy(shuffleIndex)])
+            return current + String(chars[chars.index(chars.startIndex, offsetBy: shuffleIndex)])
         }
     }
 }
